a455f51df215dcf2c4b359efbb5d96b3d849b92d
seunggeun <bsg1017@naver.com>
2022-10-06 15:20:24 +0900

    ReadFile -현재 디렉토리 파일목록

--- Java_List_1/src/java_readfile_exercise1_221006/FileExercise.java
+++ Java_List_1/src/java_readfile_exercise1_221006/FileExercise.java
@@ -15,7 +15,6 @@ public class FileExercise {
 }
 
 /*
-"C:\Program Files\Java\openjdk-18.0.1.1_windows-x64_bin\jdk-18.0.1.1\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.2\lib\idea_rt.jar=63974:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.2\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\baeks\OneDrive\바탕 화면\221006\Java_List_1\out\production\Java_List_1" java_readfile_exercise1_221006.FileExercise
 .\.idea
 .\Java_List_1.iml
 .\out
