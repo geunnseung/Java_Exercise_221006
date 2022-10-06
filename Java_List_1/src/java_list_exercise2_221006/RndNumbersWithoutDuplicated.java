843dfabc13e7c8aad449ad81d78bd49bc50164ed
seunggeun <bsg1017@naver.com>
2022-10-06 10:19:23 +0900

    RandomNumberGenerator -num까지의 숫자 랜덤으로 생성 -중복제거

--- Java_List_1/src/java_list_exercise2_221006/RndNumbersWithoutDuplicated.java
+++ Java_List_1/src/java_list_exercise2_221006/RndNumbersWithoutDuplicated.java
@@ -1,13 +1,21 @@
 package java_list_exercise2_221006;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RndNumbersWithoutDuplicated {
     public static void main(String[] args) {
 
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        HashSet<Integer> numbers= new HashSet<>();
 
         for( int i=0; i<50 ; i++) {
             int r =randomNumberGenerator.generate(25);
-            System.out.println(r);
+            numbers.add(r);
+
         }
+
+        System.out.println(numbers);
+        System.out.println(numbers.size());
     }
 }
